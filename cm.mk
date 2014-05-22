ee3ffcab72c6d659fd274fd9cdbced887c423f18
Kra1o5 <kra1o5x@gmail.com>
2014-05-22 16:52:45 +0200

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,9 +1,9 @@
 # Inherit device configuration
-$(call inherit-product, device/bq/maxwell2lite/device.mk)
+$(call inherit-product, device/bq/maxwell2/device.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := maxwell2lite
-PRODUCT_NAME := cm_maxwell2lite
+PRODUCT_DEVICE := maxwell2
+PRODUCT_NAME := cm_maxwell2
 PRODUCT_BRAND := bq
 PRODUCT_MANUFACTURER := bq
-PRODUCT_MODEL := bq Maxwell 2 Lite
+PRODUCT_MODEL := bq Maxwell 2
